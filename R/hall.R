ff66447721edb798e111034abbae40c2a7ea2d25
pvermees <p.vermeesch@ucl.ac.uk>
2022-03-06 22:57:10 +0000

    Added independent Hall regression

--- R/hall.R
+++ R/hall.R
@@ -2,36 +2,50 @@ hall <- function(a,g,k,tt){
     exp(a-g*tt) + (1-exp(-g*tt))*k/g
 }
 
-init.hall <- function(zdat,i){
+init.hall <- function(zdat,j,outliers=NULL){
     gmisfit <- function(g,a,tmiddle,cmiddle,tlast,clast){
         pred <- exp(a-g*tlast) +
             (cmiddle-exp(a-g*tmiddle))*(1-exp(-g*tlast))/(1-exp(-g*tmiddle))
         (clast - pred)^2
     }
-    nr <- nrow(zdat$sig)
-    pos <- zdat$sig[,i]>0
-    first <- zdat$sig[pos,i][1]
+    if (!is.null(outliers)){
+        sig <- zdat$sig[-outliers,]
+        tim <- zdat$tim[-outliers,]
+    } else {
+        sig <- zdat$sig
+        tim <- zdat$tim
+    }
+    nr <- nrow(sig)
+    pos <- sig[,j]>0
+    first <- sig[pos,j][1]
     a <- log(first)
-    clast <- zdat$sig[nr,i]
-    tlast <- hours(zdat$tim[nr,i])
-    cmiddle <- zdat$sig[round(nr/2),i]
-    tmiddle <- hours(zdat$tim[round(nr/2),i])
+    clast <- sig[nr,j]
+    tlast <- hours(tim[nr,j])
+    cmiddle <- sig[round(nr/2),j]
+    tmiddle <- hours(tim[round(nr/2),j])
     g <- optimise(gmisfit,interval=c(-10,10),
                   a,tmiddle,cmiddle,tlast,clast)$minimum
     k <- (cmiddle-exp(a-g*tmiddle))*g/(1-exp(-g*tmiddle))
     c(a,g,k)
 }
 
-misfit.hall <- function(par,zdat,j){
+misfit.hall <- function(par,zdat,j,outliers=NULL){
     n <- length(j)
     a <- par[1:n]
     g <- par[n+1]
     k <- par[(n+2):(2*n+1)]
     out <- 0
+    if (!is.null(outliers)){
+        sig <- zdat$sig[-outliers,]
+        tim <- zdat$tim[-outliers,]
+    } else {
+        sig <- zdat$sig
+        tim <- zdat$tim
+    }
     for (i in 1:n){
         jj <- j[i]
-        tt <- hours(zdat$tim[,jj])
-        obs <- zdat$sig[,jj]
+        tt <- hours(tim[,jj])
+        obs <- sig[,jj]
         pred <- hall(a[i],g,k[i],tt)
         if (jj %in% zdat$FAR){
             out <- sum((obs - pred)^2)
@@ -46,11 +60,43 @@ misfit.hall <- function(par,zdat,j){
     out
 }
 
-hallfit <- function(zdat){
+hallfit <- function(zdat,outliers=NULL){
+    out <- zdat
+    class(out) <- 'hall'
+    out$agk <- list()
+    n <- length(zdat$codes)
+    for (j in 1:n){
+        init <- init.hall(zdat,j,outliers=outliers)
+        lower <- c(init[1]-1,init[2]-1,init[3]/2)
+        upper <- c(init[1]+1,init[2]+1,init[3]*2)
+        code <- zdat$codes[j]
+        out$agk[[code]] <- optim(init,misfit.hall,method='L-BFGS-B',
+                                 lower=lower,upper=upper,
+                                 zdat=zdat,j=j,outliers=outliers)$par
+    }
+    out
+}
+plot.hall <- function(zdat){
+    n <- length(zdat$codes)
+    nr <- ceiling(sqrt(n))
+    nc <- ceiling(n/nr)
+    oldpar <- par(mfrow=c(nr,nc))
+    on.exit(par(oldpar))
+    for (j in 1:n){
+        agk <- zdat$agk[[j]]
+        tt <- hours(zdat$tim[,j])
+        pred <- hall(agk[1],agk[2],agk[3],tt)
+        obs <- zdat$sig[,j]
+        plot(tt,obs,ylab='y')
+        lines(tt,pred)
+    }
+}
+
+hallfitjoint <- function(zdat){
     groups <- unique(zdat$groups)
     out <- zdat
     out$agk <- list()
-    class(out) <- 'hall'
+    class(out) <- 'hall2'
     for (g in groups){
         j <- which(zdat$groups %in% g)
         n <- length(j)
@@ -66,8 +112,7 @@ hallfit <- function(zdat){
     }
     out
 }
-
-plot.hall <- function(zdat){
+plot.halljoint <- function(zdat){
     n <- length(zdat$codes)
     nr <- ceiling(sqrt(n))
     nc <- ceiling(n/nr)
